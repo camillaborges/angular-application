dd97178ad2fc554fc88629a383deaa856a04ac0f
Borges Camilla <camillaborgeshelena@gmail.com>
2021-12-01 13:48:45 +0100

    fix problem task service

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Task } from '../Task';
-import { TASKS } from '../mock.tasks';
+
 
 
 
