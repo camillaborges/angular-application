7e87591341adb5f05f32b5886e02febb5a4de5a2
Borges Camilla <camillaborgeshelena@gmail.com>
2021-12-01 14:45:50 +0100

    implementing functionality to delete icon

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -17,4 +17,9 @@ export class TaskService {
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl) 
   }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
 }
